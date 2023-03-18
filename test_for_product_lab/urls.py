35b2d2419bc49aaaacb5d2c381c7c5ad78500059
korneyka3000 <korneyburau@gmail.com>
2023-03-18 18:51:52 +0300

    docker-compose conf and url routing changes

--- test_for_product_lab/urls.py
+++ test_for_product_lab/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('api/', include('api.urls')),
+    path('product_lab/', index, name='index'),
+    path('product_lab/api/', include('api.urls')),
 ]
