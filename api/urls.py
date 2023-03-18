839b3a93ec00c9647221733f50e7932340bfce65
korneyka3000 <korneyburau@gmail.com>
2023-03-18 18:59:09 +0300

    url routing reworked

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from .views import index, ProductUploadView
 
-from .views import ProductUploadView
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Wildberries Product Upload API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('upload/', ProductUploadView.as_view(), name='upload'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', index, name='index'),
+    path('api/upload/', ProductUploadView.as_view(), name='upload'),
 ]
--- test_for_product_lab/urls.py
+++ test_for_product_lab/urls.py
@@ -13,29 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 
-from api.views import index
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Wildberries Product Upload API",
-      default_version='v1',
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+from django.urls import include, path
 
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
-    path('product_lab/', index, name='index'),
-    path('product_lab/api/', include('api.urls')),
+    path('product_lab/', include('api.urls')),
 ]
